1e0036a638deb791e6a0632786108cc7859a720f
misbahul45 <misbahulmuttaqin395@gmail.com>
2024-02-10 14:05:27 +0700

    mhd

--- src/components/messages/Sub/UsersMessages.jsx
+++ src/components/messages/Sub/UsersMessages.jsx
@@ -7,7 +7,11 @@ const UsersMessages = ({ uImage, uName, uFriendId, isFirstIndex, isLastIndex }) 
    const route=useRoute(state=>state.route)
   return (
     <div onClick={()=>changeRoute(`/messages/message/${uFriendId}`)} className={`${route.includes(uFriendId)?"bg-green-700":"bg-white/5"} flex items-center gap-4 py-2.5 pl-6 cursor-pointer font-semibold hover:bg-green-600 transition-all duration-200 ${isFirstIndex?"rounded-t-lg":isLastIndex?"rounded-b-lg":""}`}>
-      <img className="w-10 h-10 rounded-full" src={uImage} alt="friend user image" />
+      {uImage?
+        <img className="w-10 h-10 rounded-full" src={uImage} alt="friend user image" />
+        :
+        <div></div>
+      }
       <h1 className="text-lg text-slate-100">{uName}</h1>
     </div>
   )
